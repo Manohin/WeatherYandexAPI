66822a1245763315a60bd7de434f0783de4c0ec4
Manohin <111537419+Manohin@users.noreply.github.com>
2023-06-03 17:32:19 +0300

    1

--- WeatherYandexAPI/ViewControllers/WeatherViewController.swift
+++ WeatherYandexAPI/ViewControllers/WeatherViewController.swift
@@ -11,28 +11,6 @@ import SVGKit
 
 final class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     
-    private let weatherConditions: [String: String] = [
-        "overcast": "облачно",
-        "clear": "ясная погода",
-        "partly-cloudy": "переменная облачность",
-        "drizzle": "морось",
-        "light-rain": "идет небольшой дождь",
-        "rain": "идет дождь",
-        "moderate-rain": "идет умеренно сильный дождь",
-        "heavy-rain": "идет сильный дождь",
-        "continuous-heavy-rain": "идет длительный сильный дождь",
-        "showers": "ливень",
-        "wet-snow": "идет дождь со снегом",
-        "light-snow": "идет небольшой снег",
-        "snow": "идет снег",
-        "snow-showers": "снегопад",
-        "hail": "идет град",
-        "thunderstorm": "гроза",
-        "thunderstorm-with-rain": "идет дождь с грозой",
-        "thunderstorm-with-hail": "гроза с градом",
-        "cloudy": "облачно с прояснениями",
-    ]
-    
     var latitude: Double = 0.0
     var longitude: Double = 0.0
     var locationManager: CLLocationManager!
@@ -47,7 +25,6 @@ final class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
-        fetchDataFromAPI()
         recommendationsLabel.textColor = .systemBlue
         
         // Инициализация CLLocationManager
@@ -100,34 +77,16 @@ final class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     }
     
     private func fetchDataFromAPI() {
-        guard let url = URL(string: "https://api.weather.yandex.ru/v2/informers?lat=\(latitude)&lon=\(longitude)") else {
-            return
-        }
-        
-        var request = URLRequest(url: url)
-        request.addValue("937ce0f2-8247-49e0-9d9a-c4bdaeab8f93", forHTTPHeaderField: "X-Yandex-API-Key")
-        
-        URLSession.shared.dataTask(with: request) { (data, response, error) in
-            if let error = error {
-                print("Ошибка при выполнении запроса: \(error.localizedDescription)")
+        NetworkManager.shared.fetchData(for: latitude, longitude: longitude) { [weak self] weatherData in
+            guard let self = self, let weatherData = weatherData else {
                 return
             }
             
-            guard let data = data else {
-                print("Данные пустые")
-                return
+            DispatchQueue.main.async {
+                self.updateUI(with: weatherData)
+                self.activityIndicator.stopAnimating()
             }
-            do {
-                let weatherData = try JSONDecoder().decode(WeatherData.self, from: data)
-                
-                DispatchQueue.main.async {
-                    self.updateUI(with: weatherData)
-                    self.activityIndicator.stopAnimating()
-                }
-            } catch {
-                print("Ошибка при декодировании данных: \(error.localizedDescription)")
-            }
-        }.resume()
+        }
     }
     
     private func updateUI(with weatherData: WeatherData) {
@@ -143,28 +102,26 @@ final class WeatherViewController: UIViewController, CLLocationManagerDelegate {
             }
         }
         
-        switch weatherData.fact.feelsLike {
-        case 1...10:
-            recommendationsLabel.text = "Холодно. Наденьте легкую теплую куртку или пуловер, утепленные брюки или джинсы"
-        case 11...17:
-            recommendationsLabel.text = "Прохладно. Наденьте легкую куртку или ветровку, брюки или джинсы"
-        case 18...27:
-            recommendationsLabel.text = "Тепло. Выбирайте легкую одежду: Футболку, легкие брюки или шорты. Используйте легкую обувь, такую как сандалии или кроссовки."
-        case 28...35:
-            recommendationsLabel.text = "Жарко. Носите легкую и воздухопроницаемую одежду. Используйте шорты, футболки, топы или платья из легких материалов. Оптимально выбирать светлые цвета, которые отражают солнечные лучи. Наденьте шляпу или кепку, чтобы защититься от солнца. Используйте удобную и дышащую обувь, например, сандалии или открытые туфли."
-        case 36...:
-            recommendationsLabel.text = "Очень жарко. По возможности, оставайтесь в прохладном помещении. При выходе на улицу одевайтесь очень легко, пейте больше воды и старайтесь проводить меньше времени под прямыми солнечными лучами"
-        default:
-            recommendationsLabel.text = "Мороз. Наденьте теплую куртку (пуховик, пальто), шапку, теплую обувь, теплые штаны."
-        }
-        
+        recommendationsLabel.text = getRecommendations(for: Double(weatherData.fact.feelsLike))
         tempLabel.text = "Температура: \(weatherData.fact.temp)°C"
         feelsLikeLabel.text = "Ощущается как: \(weatherData.fact.feelsLike)°C"
-        
-        if let condition = weatherConditions[weatherData.fact.condition] {
-            conditionLabel.text = "На улице" + " " + condition
-        } else {
-            conditionLabel.text = weatherData.fact.condition
+        conditionLabel.text = NetworkManager.shared.getConditionLabel(for: weatherData.fact.condition)
+    }
+    
+    private func getRecommendations(for feelsLike: Double) -> String {
+        switch feelsLike {
+        case 1...10:
+            return "Холодно. Наденьте легкую теплую куртку или пуловер, утепленные брюки или джинсы"
+        case 11...17:
+            return "Прохладно. Наденьте легкую куртку или ветровку, брюки или джинсы"
+        case 18...27:
+            return "Тепло. Выбирайте легкую одежду: Футболку, легкие брюки или шорты. Используйте легкую обувь, такую как сандалии или кроссовки."
+        case 28...35:
+            return "Жарко. Носите легкую и воздухопроницаемую одежду. Используйте шорты, футболки, топы или платья из легких материалов. Оптимально выбирать светлые цвета, которые отражают солнечные лучи. Наденьте шляпу или кепку, чтобы защититься от солнца. Используйте удобную и дышащую обувь, например, сандалии или открытые туфли."
+        case 36...:
+            return "Очень жарко. По возможности, оставайтесь в прохладном помещении. При выходе на улицу одевайтесь очень легко, пейте больше воды и старайтесь проводить меньше времени под прямыми солнечными лучами"
+        default:
+            return "Мороз. Наденьте теплую куртку (пуховик, пальто), шапку, теплую обувь, теплые штаны."
         }
     }
     
